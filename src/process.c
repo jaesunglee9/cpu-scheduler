92ab7838565b39c99174414cdd07afed9b7e50a9
Jaesung Lee <ecumenepolis@gmail.com>
2024-05-19 15:53:13 +0900

    add run_process() in src/process.c

--- src/process.c
+++ src/process.c
@@ -1,1 +1,11 @@
- 
+pcb* create_pcb(unsigned int pid){
+    if (pid==0){
+        return NULL;
+    }
+
+}
+
+void run(pcb* process,unsigned int runtime){
+    process->cpu_remaining -= runtime;
+    return;
+}
