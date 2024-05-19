f37e2cf7038b68cfa46bb0a0e785c9c056e2955e
Jaesung Lee <ecumenepolis@gmail.com>
2024-05-19 16:04:48 +0900

    change process.c accordingly

--- src/process.c
+++ src/process.c
@@ -1,13 +1,12 @@
-pcb* create_pcb(unsigned int pid){
-    if (pid==0){
-        return NULL;
-    }
+pcb_t* initialize_pcb(unsigned int pid){
+    pcb_t* pcb= (pcb_t*)malloc(sizeof(pcb_t));
+    pcb
 
 }
 
 // This is simply a decrement function for the given pcb. The amount of time that should be decremented is determined by the cpu scheduler.
-void run_process(pcb* process,unsigned int runtime){
-    process->cpu_remaining -= runtime;
+void run_process(pcb_t* pcb,unsigned int runtime){
+    pcb->cpu_remaining -= runtime;
     return;
 }
 
