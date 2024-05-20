4a9096ec5e9d001ff18b10e70360b94109a9f46b
Jaesung Lee <ecumenepolis@gmail.com>
2024-05-20 10:51:12 +0900

    add del_process() on process.c

--- src/process.c
+++ src/process.c
@@ -16,13 +16,20 @@ pcb_t* initialize_rand_pcb(unsigned int pid){
     pcb->cpu_remaining = pcb->cpu_burst;
     pcb->io_burst = rand()%MAXBURST;
     pcb->io_remaining = pcb->io_burst;
-
+    pcb->completion = FALSE;
 }
+
+// TODO initialize according to some determined table()
+
 
 // This is simply a decrement function for the given pcb. The amount of time that should be decremented is determined by the cpu scheduler.
 void run_process(pcb_t* pcb,unsigned int runtime){
     pcb->cpu_remaining -= runtime;
     return;
+}
+
+void del_process (pcb_t* process) {
+    free(process);
 }
 
 
