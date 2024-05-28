1e70ffba65e0f00b66aecaf89e0e6df82ca970d3
Jaesung Lee <ecumenepolis@gmail.com>
2024-05-28 12:56:39 +0900

    update init_rand_pcb
    reserve pid 0 for log end of compute, but even if further change comes handle on their end.

--- src/process.c
+++ src/process.c
@@ -1,35 +1,43 @@
 #include <stdlib.h>
 
+#include "./process.h"
+
 #define MAXBURST 100
 #define MAXARRIVAL  100
 #define MAXPRIORITY 100
 
 
 
-pcb_t* initialize_rand_pcb(unsigned int pid){
-    pcb_t* pcb= (pcb_t*)malloc(sizeof(pcb_t));
+void
+init_rand_pcb(pcb_t pcb[], int len)
+{
+    int i;
+    for (i = 0; i < len; i++){
 
-    pcb->pid = pid;
-    pcb->priority = rand()%MAXPRIORITY;
-    pcb->arrival = rand()%MAXARRIVAL;
-    pcb->cpu_burst = rand()%MAXBURST;
-    pcb->cpu_remaining = pcb->cpu_burst;
-    pcb->io_burst = rand()%MAXBURST;
-    pcb->io_remaining = pcb->io_burst;
-    pcb->completion = FALSE;
+        pcb[i].pid = i+1;  /* 0 is reserved for end of computation in cpu log. This should change if cpu can be idle during computation.*/
+        pcb[i].priority = rand()%MAXPRIORITY;
+
+        pcb[i].arrival = rand()%MAXARRIVAL;
+        pcb[i].cpu_burst = rand()%MAXBURST;
+        pcb[i].cpu_remaining = pcb->cpu_burst;
+
+        pcb->completion = 0;
+    }
 }
 
 // TODO initialize according to some determined table()
 
 
-// This is simply a decrement function for the given pcb. The amount of time that should be decremented is determined by the cpu scheduler.
+// This is simply a decrement function for the given pcb when it is run on cpu. The amount of time that should be decremented is determined by the cpu scheduler.
 void run_process(pcb_t* pcb,unsigned int runtime){
     pcb->cpu_remaining -= runtime;
     return;
 }
 
-void del_process (pcb_t* process) {
-    free(process);
-}
+// I am not sure we require io burst in cpu simulation.
 
+// void del_process (pcb_t* process) {
+//     free(process);
+// }
+//
 
